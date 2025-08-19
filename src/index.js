74dc60ed995a8426a03c7dba780ac3af60e2c444
Aryan Parmar <notaarryan@users.noreply.github.com>
2025-08-19 17:21:26 -0400

    feat: completed nav bar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "./style.css";
+
+const toggleLightingModes = document.getElementById("toggleLightingModes");
+
+toggleLightingModes.addEventListener("click", () => {
+  document.documentElement.classList.toggle("dark");
+  if (document.documentElement.classList.contains("dark")) {
+    toggleLightingModes.innerText = "Light Mode";
+  } else {
+    toggleLightingModes.innerText = "Dark Mode";
+  }
+});
