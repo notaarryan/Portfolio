014dd53baf65d6041269ee0a2c2de12fae1d26f9
Aryan Parmar <notaarryan@users.noreply.github.com>
2025-08-19 21:16:45 -0400

    feat: completed hero section

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./style.css";
 
 const toggleLightingModes = document.getElementById("toggleLightingModes");
+const cards = document.querySelectorAll("div.cards");
+const heroSection = document.querySelector("section.hero-section");
 
 toggleLightingModes.addEventListener("click", () => {
   document.documentElement.classList.toggle("dark");
@@ -9,4 +11,38 @@ toggleLightingModes.addEventListener("click", () => {
   } else {
     toggleLightingModes.innerText = "Dark Mode";
   }
+});
+
+const rotateCards = () => {
+  let angle = 0;
+  cards.forEach((card, zIndex) => {
+    if (!card.classList.contains("away")) {
+      card.style.transform = `translateY(0) rotate(${angle}deg)`;
+      card.style.zIndex = cards.length - zIndex;
+      angle = angle - 10;
+    } else if (card.classList.contains("away")) {
+      card.style.transform = `translateY(-120vh) rotate(48deg)`;
+    } else {
+      card.style.transform = `translateY(120vh) rotate(${angle}deg)`;
+    }
+  });
+};
+
+rotateCards();
+
+window.addEventListener("scroll", () => {
+  let distance = window.innerHeight / 5;
+  let topValue = heroSection.getBoundingClientRect().top;
+  let index = -1 * (topValue / distance + 1);
+  index = Math.floor(index);
+  for (let i = 0; i < cards.length; i++) {
+    if (i <= index) {
+      cards[i].classList.add("away");
+    } else {
+      cards[i].classList.remove("away");
+      let angle = -10 * i;
+      cards[i].style.transform = `translateY(120vh) rotate(${angle}deg)`;
+    }
+  }
+  rotateCards();
 });
