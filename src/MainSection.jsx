e163e750a4e89c61180534497e54394a5603759e
Aryan Parmar <156536547+notaarryan@users.noreply.github.com>
2025-10-28 02:24:01 -0400

    feat: completed implementation of AboutSection

--- src/MainSection.jsx
+++ src/MainSection.jsx
@@ -1,10 +1,12 @@
 import HeroSection from "./HeroSection";
+import AboutSection from "./AboutSection";
 import "./MainSection.css";
 
 function MainSection({ setShowScrollButton }) {
   return (
     <main>
       <HeroSection setShowScrollButton={setShowScrollButton} />
+      <AboutSection />
     </main>
   );
 }
